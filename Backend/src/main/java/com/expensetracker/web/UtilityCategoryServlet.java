efd983471a80927f36b2397cb4322c466797e132
Aleksandr Kochutov <alex.kochutov@gmail.com>
2024-04-21 19:46:34 +0400

    BE: Add new method to UtilityCategoryServlet.java. Defined methods: 	1. doPost (handles POST-requests for saving UtilityCategory objects).

--- Backend/src/main/java/com/expensetracker/web/UtilityCategoryServlet.java
+++ Backend/src/main/java/com/expensetracker/web/UtilityCategoryServlet.java
@@ -1,10 +1,13 @@
 package com.expensetracker.web;
 
+import com.expensetracker.model.UtilityCategory;
 import com.expensetracker.service.UtilityCategoryService;
+import com.google.gson.Gson;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -13,11 +16,40 @@ public class UtilityCategoryServlet extends HttpServlet {
     private static final UtilityCategoryService UC_SERVICE = new UtilityCategoryService();
 
     @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        request.setCharacterEncoding("utf-8");
+        response.setHeader("Content-type", "application/json; charset=utf-8");
+
+        String payload = "";
+        String pathInfo = request.getPathInfo();
+
+        if (checkEndpoint(pathInfo)) {
+            Gson gson = new Gson();
+            UtilityCategory category = gson.fromJson(getRequestBody(request), UtilityCategory.class);
+            if (Objects.isNull(category.getName())) {
+                response.setStatus(400);
+                payload = "{ \"errorMessage\" : \"Mandatory field 'id' is not found\" }";
+            } else {
+                payload = UC_SERVICE.saveCategory(category);
+                if (payload.contains("errorMessage"))
+                    response.setStatus(400);
+                if (payload.contains("result"))
+                    response.setStatus(200);
+            }
+        }
+        response.getWriter().write(payload);
+    }
+
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setHeader("Content-type", "application/json; charset=utf-8");
+
         String payload;
         String pathInfo = request.getPathInfo();
-        response.setHeader("Content-type", "application/json; charset=utf-8");
-        if (Objects.isNull(pathInfo) || Objects.equals(pathInfo, "/")) {
+
+        if (checkEndpoint(pathInfo)) {
             response.setStatus(200);
             payload = UC_SERVICE.getAllCategories();
         } else {
@@ -37,5 +69,22 @@ public class UtilityCategoryServlet extends HttpServlet {
             }
         }
         response.getWriter().write(payload);
+    }
+
+    private boolean checkEndpoint(String pathInfo) {
+        return Objects.isNull(pathInfo) || Objects.equals(pathInfo, "/");
+    }
+
+    private String getRequestBody(HttpServletRequest request) {
+        try (BufferedReader reader = request.getReader()) {
+            StringBuilder body = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                body.append(line);
+            }
+            return body.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
